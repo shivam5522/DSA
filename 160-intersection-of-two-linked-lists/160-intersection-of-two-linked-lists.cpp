7b67dc089dd72f95a65c3ed46c2b2db14367da33
Shivam Thakkar <66198514+shivam5522@users.noreply.github.com>
2022-07-11 08:08:09 +0530

    Time: 54 ms (78.02%), Space: 14.5 MB (88.34%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,28 +9,40 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        bool flag=false;
-        int v=0;
-        map<ListNode*,int> track;
-        while(headA!=NULL){
-            track[headA];
-            headA=headA->next;
+        int diff=0;
+        int len1=0,len2=0;
+        ListNode* t1=headA;
+        ListNode* t2=headB;
+        while(t1!=NULL){
+            len1++;
+            t1=t1->next;
         }
-        while(headB!=NULL){
-            if(track.count(headB)){
-                flag=true;
-                v=headB->val;
-                break;
+        while(t2!=NULL){
+            len2++;
+            t2=t2->next;
+        }
+        if(len1>len2){
+            diff=len1-len2;
+            while(diff!=0){
+                headA=headA->next;
+                diff--;
             }
-            track[headB];
+        }
+        else if(len1==len2){
+            ;
+        }
+        else{
+            diff=len2-len1;
+            while(diff!=0){
+                headB=headB->next;
+                diff--;
+            }
+        }
+        while(headA!=NULL && headB!=NULL){
+            if(headA==headB) return headA;
+            headA=headA->next;
             headB=headB->next;
         }
-        // if(flag){
-        //     cout<<"Intersected at "<<v;
-        // }
-        // else{
-        //     cout<<"No intersection";
-        // }
-        return headB;
+        return headA;
     }
 };
