5670415776eb4cbfe55f9ae1a4b9d3f37f51d653
Shivam Thakkar <66198514+shivam5522@users.noreply.github.com>
2022-07-13 12:14:54 +0530

    Time: 81 ms (15.74%), Space: 72.5 MB (33.30%) - LeetHub

--- 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
+++ 445-add-two-numbers-ii/445-add-two-numbers-ii.cpp
@@ -11,18 +11,59 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        string a, b;
-        ListNode *result = nullptr;
-        while(l1) { a.push_back(l1->val+'0'); l1 = l1->next;}
-        while(l2) { b.push_back(l2->val+'0'); l2 = l2->next;}
-        int l = a.size()-1, r = b.size()-1, carry = 0;
-        while(l >= 0 || r >= 0 || carry == 1) {
-            int c = (l >= 0 ? a[l--]-'0' : 0) + ( r >= 0 ? b[r--]-'0' : 0) + carry;
-            ListNode *temp = new ListNode(c%10);
-            temp->next = result;
-            result = temp;
-            carry = c/10;
-        }        
-        return result;
+        stack<int> temp;
+        int len1=0,len2=0;
+        ListNode* t1=l1;
+        ListNode* t2=l2;
+        while(t1!=NULL){
+            len1++;
+            t1=t1->next;
+        }
+        while(t2!=NULL){
+            len2++;
+            t2=t2->next;
+        }
+        while(len1>len2){
+            temp.push(l1->val);
+            l1=l1->next;
+            len1--;
+        }
+        while(len1<len2){
+            temp.push(l2->val);
+            l2=l2->next;
+            len2--;
+        }
+        while(l1!=NULL){
+            temp.push(l1->val+l2->val);
+            l1=l1->next;
+            l2=l2->next;
+        }
+        ListNode* final=new ListNode();
+        int carry=0;
+        int count=0;
+        while(!temp.empty()){
+            int v=temp.top()+carry;
+            temp.pop();
+            carry=0;
+            if(v>=10){
+                v=v%10;
+                carry=1;
+            }
+            ListNode* t=new ListNode(v);
+            if(count==0){
+                final=t;
+            }
+            else{
+                t->next=final;
+                final=t;
+            }
+            count++;
+        }
+        if(carry==1){
+            ListNode* f=new ListNode(1);
+            f->next=final;
+            final=f;
+        }
+        return final;
     }
 };
